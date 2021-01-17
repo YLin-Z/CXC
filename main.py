284b25f50dec854fb9859205f115241beb6d1147
YLin-Z <1615200012@e.gzhu.edu.cn>
2021-01-17 19:32:26 +0800

    update time select method

--- main.py
+++ main.py
@@ -18,7 +18,10 @@ def dongshapage():
         time = request.args.get('time')     #得到连接中的时间信息
         mapdata = model.dongsha_map(time)    #由时间得地图上的流量数据
     if request.method == 'POST':
-        time = request.form.get('time')
+        if request.form.get('time')=="dateandtimepoint":#当传回的时间是这个串时说明是地图右边的表单，需要日期和小时拼接成time
+            time = request.form.get('date')+" "+request.form.get('timepoint')
+        else:
+            time = request.form.get('time')
         mapdata = model.dongsha_map(time)
     return render_template("dongsha.html",data=mapdata)
 
