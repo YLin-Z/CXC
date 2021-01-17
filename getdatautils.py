7a982858443a12b7407c29ba0b87395bd12c6cd1
YLin-Z <1615200012@e.gzhu.edu.cn>
2021-01-17 22:42:30 +0800

    updating nanjiaocun

--- getdatautils.py
+++ getdatautils.py
@@ -27,3 +27,36 @@ def getdongsha_shiliuliang(time):
             # print(data)
             del sheet
     return data
+
+def nanjiaocun_shiliuliang(time):
+    if time == ' 0:00':  # 在右边表单那里没选好
+        data = {"error": 1}
+    else:
+        sheet = pd.read_excel("D:/CXCDATA/shaluo_nanjiao.xls", sheet_name=1)
+        # sheet = sheet.reindex(columns=['时间',1,2,3,4,5,6,7,8,9,10,11,12])
+        print(sheet.iloc[1])
+        print(sheet.iloc[2])
+        print(sheet.iloc[3])
+        print(sheet.iloc[4])
+        print(sheet.loc[1])
+        print(sheet.loc[2])
+        print(sheet.loc[3])
+        print(sheet.loc[4])
+        row = sheet[sheet['时间'] == time]#刚好日期那列上面写着南漖村
+        if row.empty == 1:
+            data = {"error": 1}
+        else:
+            print(row)
+            sll91830730 = format(row.iloc[-1][3], '.2f')
+            sll91830729 = format(row.iloc[-1][7], '.2f')
+            sll69983071 = format(row.iloc[-1][11], '.2f')
+            sunsllofnanjiaocun = format(row.iloc[-1][12], '.2f')
+            data = {'time': time,
+                    'sll91830730': sll91830730,
+                    'sll91830729': sll91830729,
+                    'sll69983071': sll69983071,
+                    'sunsllofnanjiaocun': sunsllofnanjiaocun,
+                    'error': 0}
+            # print(data)
+            del sheet
+    return data
--- model.py
+++ model.py
@@ -2,5 +2,11 @@ import getdatautils #导入数据层代码
 
 # 得到东沙地图上的流量计数据
 def dongsha_map(time):
-    mapdata = getdatautils.getdongsha_shiliuliang(time)     #从EXCEL中得到时流量
+    mapdatads = getdatautils.getdongsha_shiliuliang(time)     #从EXCEL中得到时流量
+    mapdatanjc = getdatautils.nanjiaocun_shiliuliang(time)     #从EXCEL中得到时流量
+    if mapdatads['error']==0 and mapdatanjc['error']==0:
+        mapdata = mapdatads.copy()
+        mapdata.update(mapdatanjc)
+    else:
+        mapdata = {'error':1}
     return mapdata
